42dc0157eed3c9d466e3ae4f5ab8516fcf805f2a
Camila Elizabeth <camilathbenitezmarin@gmail.com>
2025-11-10 17:34:40 -0300

    Add files via upload

--- visualizador/algorithms/sort_selection.py
+++ visualizador/algorithms/sort_selection.py
@@ -17,12 +17,25 @@ def init(vals):
     fase = "buscar"
 
 def step():
-    # TODO:
-    # - Fase "buscar": comparar j con min_idx, actualizar min_idx, avanzar j.
-    #   Devolver {"a": min_idx, "b": j_actual, "swap": False, "done": False}.
-    #   Al terminar el barrido, pasar a fase "swap".
-    # - Fase "swap": si min_idx != i, hacer ese único swap y devolverlo.
-    #   Luego avanzar i, reiniciar j=i+1 y min_idx=i, volver a "buscar".
-    #
-    # Cuando i llegue al final, devolvé {"done": True}.
-    return {"done": True}
+    global items, n, i, j, min_idx, fase
+    
+    if i >= n - 1:
+        return {"done": True}
+
+    if fase == "buscar":
+        
+        if j < n:
+            a = min_idx
+            b = j
+            
+            if items[j] < items[min_idx]:
+                min_idx = j
+                a = min_idx
+            
+            j += 1
+            
+            return {"a": a, "b": b, "swap": False, "done": False}
+        
+        else:
+            fase = "swap"
+            return {"a": i, "b": min_idx, "swap": False, "done": False}
