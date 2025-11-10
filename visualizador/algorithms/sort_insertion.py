b7fd8391a768d3bba22e855ec7a6f844efcf6507
Camila Elizabeth <camilathbenitezmarin@gmail.com>
2025-11-10 17:32:29 -0300

    Add files via upload

--- visualizador/algorithms/sort_insertion.py
+++ visualizador/algorithms/sort_insertion.py
@@ -1,41 +1,39 @@
-# Contrato: init(vals), step() -> {"a": int, "b": int, "swap": bool, "done": bool}
+ Contrato: init(vals), step() -> {"a": int, "b": int, "swap": bool, "done": bool}
 
 items = []
 n = 0
-i = 0          # cabeza de la parte no ordenada
-j = 0          # cursor que recorre y busca el mínimo
-min_idx = 0    # índice del mínimo de la pasada actual
-fase = "buscar"  # "buscar" | "swap"
+i = 0      # elemento que queremos insertar
+j = None   # cursor de desplazamiento hacia la izquierda (None = empezar)
 
 def init(vals):
-    global items, n, i, j, min_idx, fase
+    global items, n, i, j
     items = list(vals)
     n = len(items)
-    i = 0
-    j = i + 1
-    min_idx = i
-    fase = "buscar"
+    i = 1      # común: arrancar en el segundo elemento
+    j = None
 
 def step():
-    global items, n, i, j, min_idx, fase
-    
-    if i >= n - 1:
+    # TODO
+    global items, n, i, j
+    # - Si i >= n: devolver {"done": True}.
+    if i >= n:
         return {"done": True}
+    # - Si j es None: empezar desplazamiento para el items[i] (p.ej., j = i) y devolver un highlight sin swap
+    if j is None:
+        j = i #indice de la lista
+        a= j-1 #adyacente(izquierda)
+        b=j
+        return {"a": a, "b": b, "swap": False, "done": False}
+   
+    # - Mientras j > 0 y items[j-1] > items[j]: hacer UN swap adyacente (j-1, j) y devolverlo con swap=True
+    
+    if j > 0 and items[j-1] > items[j]: #comparo indices, de izquierda a derecha
+        a=j-1 
+        b=j
+        items[a], items[b] = items[b], items[a] #intercambio 
+        j = j-1
+        return {"a": a, "b": b, "swap": True, "done": False}
 
-    if fase == "buscar":
-        
-        if j < n:
-            a = min_idx
-            b = j
-            
-            if items[j] < items[min_idx]:
-                min_idx = j
-                a = min_idx
-            
-            j += 1
-            
-            return {"a": a, "b": b, "swap": False, "done": False}
-        
-        else:
-            fase = "swap"
-            return {"a": i, "b": min_idx, "swap": False, "done": False}
+    # - Si ya no hay que desplazar: avanzar i y setear j=None.
+    i = i + 1 #siguente indice
+    j = None
