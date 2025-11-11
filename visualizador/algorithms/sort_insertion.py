6719bddd1e10affe92afb2d7a232adfa4285b2d8
Camila Elizabeth <camilathbenitezmarin@gmail.com>
2025-11-11 17:06:12 -0300

    Add files via upload

--- visualizador/algorithms/sort_insertion.py
+++ visualizador/algorithms/sort_insertion.py
@@ -1,4 +1,4 @@
-items = []
+tems = []
 n = 0
 i = 0      # elemento que queremos insertar
 j = None   # cursor de desplazamiento hacia la izquierda (None = empezar)
@@ -11,31 +11,34 @@ def init(vals):
     j = None
 
 def step():
-    global items, n, i, j
+    global items, n, i, j 
 
-  # - Si i >= n: devolver {"done": True}.
+    
+    # Si ya recorrimos todos los elementos, terminamos
     if i >= n:
         return {"done": True}
-
+    
+    # Si j es None, iniciamos desplazamiento para el elemento en posición i
     if j is None:
         j = i
         a = j - 1
         b = j
         return {"a": a, "b": b, "swap": False, "done": False}
 
-       # - Mientras j > 0 y items[j-1] > items[j]: hacer UN swap adyacente (j-1, j) y devolverlo con swap=True
+    # Calculamos a y b en cada paso antes de comparar
+    a = j - 1
+    b = j
 
-    if j > 0 and items[a] > items[b]:
-        a = j - 1
-        b = j
-        items[a], items[b] = items[b], items[a]
-        j -= 1
+    # Si el elemento anterior es mayor, hacemos un swap
+    if j > 0 and items[a] > items[b]: #comparacion
+        items[a], items[b] = items[b], items[a] #intercambio
+        j = j - 1
         return {"a": a, "b": b, "swap": True, "done": False}
 
-#Si ya no hay que desplazar: avanzar i y setear j=None.
-
-    i = i+ 1
+    # Si no hay que desplazar más, avanzamos al siguiente elemento
+    a = j - 1
+    b = j
+    i = i + 1
     j = None
-    a = i - 1
-    b = i
+   
     return {"a": a, "b": b, "swap": False, "done": False}
