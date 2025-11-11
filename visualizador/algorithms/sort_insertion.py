bd5c3d2d866b866df1d8277637ee1b9febbdcd3a
Camila Elizabeth <camilathbenitezmarin@gmail.com>
2025-11-11 00:12:47 -0300

    Add files via upload

--- visualizador/algorithms/sort_insertion.py
+++ visualizador/algorithms/sort_insertion.py
@@ -1,4 +1,3 @@
-# Contrato: init(vals), step() -> {"a": int, "b": int, "swap": bool, "done": bool}
 items = []
 n = 0
 i = 0      # elemento que queremos insertar
@@ -8,17 +7,14 @@ def init(vals):
     global items, n, i, j
     items = list(vals)
     n = len(items)
-    i = 1 # común: arrancar en el segundo elemen
+    i = 1  # empezamos desde el segundo elemento
     j = None
 
 def step():
     global items, n, i, j
 
-     # - Si i >= n: devolver {"done": True}.
     if i >= n:
         return {"done": True}
-
-    # - Si j es None: empezar desplazamiento para el items[i] (p.ej., j = i) y devolver un highlight sin swap
 
     if j is None:
         j = i
@@ -26,17 +22,14 @@ def step():
         b = j
         return {"a": a, "b": b, "swap": False, "done": False}
 
-      # - Mientras j > 0 y items[j-1] > items[j]: hacer UN swap adyacente (j-1, j) y devolverlo con swap=True
-    
+    a = j - 1
+    b = j
     if j > 0 and items[a] > items[b]:
         items[a], items[b] = items[b], items[a]
-        a = j - 1
-        b = j
-        j=j-1
+        j -= 1
         return {"a": a, "b": b, "swap": True, "done": False}
 
-#Si ya no hay que desplazar: avanzar i y setear j=None.
-    i =i+1
+    i += 1
     j = None
     a = i - 1
     b = i
